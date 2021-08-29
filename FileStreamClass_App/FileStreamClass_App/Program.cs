44975c3ec3e79a366945e0df0e43248bfe2b3e02
m-a-d-e-e <88501808+m-a-d-e-e@users.noreply.github.com>
2021-08-29 15:49:28 +0530

    Add files via upload

---
+++ FileStreamClass_App/FileStreamClass_App/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileStreamClass_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Madeeha.Shaikh\source\repos\Training (Bhavana maam)\File IO\MyFileStream.txt";
+
+            // Delete the file if it exists.
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            int i;
+            FileStream f = new FileStream(path, FileMode.OpenOrCreate);
+            for (i = 65; i <= 70; i++)
+            {
+                f.WriteByte((byte)i);
+            }
+
+            f.Seek(0, SeekOrigin.Begin);
+
+            while ((i = f.ReadByte()) != -1)
+            {
+                Console.WriteLine((char)i);
+            }
+
+            f.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
